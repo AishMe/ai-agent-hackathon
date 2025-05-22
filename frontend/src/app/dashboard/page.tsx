ac18e6d1ddb0251385a19c17c83915d424f601e5
Aishwarya Anil Menon <aishwarya.menon.15@gmail.com>
2025-05-22 18:53:49 +0530

    organizing the pages

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,10 +1,15 @@
-export default function Home() {
+import ChatHelp from "@/components/ChatHelp";
+import ChatInput from "@/components/ChatInput";
+
+export default function Dashboard() {
   return (
       <main className="min-h-screen flex flex-col items-center justify-center px-2">
         <div className="mx-auto flex flex-col items-center gap-5 w-full">
           <h2 className="text-xl md:text-3xl font-semibold text-white px-4">
-            Welcome!
+            What can I help you with?
           </h2>
+          <ChatInput />
+          <ChatHelp />
         </div>
       </main>
   )
