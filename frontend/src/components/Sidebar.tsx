85a0a82c3a111575c23abdc46ff127a91e4fa384
Aishwarya Anil Menon <aishwarya.menon.15@gmail.com>
2025-05-22 14:59:48 +0530

    add toggle button

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import NewChat from './NewChat'
+import { IoSunny } from 'react-icons/io5'
 
 const Sidebar = () => {
   return (
-    <div>
-        <NewChat />
+    <div className="hidden md:flex flex-col h-screen relative p-2.5">
+        <div className="flex items-center gap-1">
+            <button 
+                className="p-1.5 md:p-2 border border-gray-600 hover:border-white/50 hover:bg-white/30 rounded-lg duration-300">
+                    <IoSunny size={20}/>
+            </button>
+            <NewChat />
+        </div>
     </div>
   )
 }
